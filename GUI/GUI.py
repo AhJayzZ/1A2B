76fcb2cc0a801c8dd66b5731371a577595d85e97
AhJayzZ <antony95070@gmail.com>
2021-10-18 03:32:24 +0800

    GUI Finished

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -11,6 +11,7 @@ class MainWindow(QtWidgets.QMainWindow,Ui_GameGUI):
         super(MainWindow,self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('1A2B Game v1.0')
+        self.setWindowIcon(QtGui.QIcon('GUI/image/app_icon.png'))
         self.MsgBox = QtWidgets.QMessageBox()
         self.MsgBox.setWindowTitle('訊息')
 
@@ -18,45 +19,59 @@ class MainWindow(QtWidgets.QMainWindow,Ui_GameGUI):
         self.answer_label.setStyleSheet("Color:Red")
         self.answer_label.setHidden(True)
         self.input_textbox.setMaxLength(4)
-        self.input_textbox.setFocus()
 
         # button trigger
         self.exit_btn.clicked.connect(sys.exit)
         self.confirm_btn.clicked.connect(self.confirm_btn_click)
         self.reset_btn.clicked.connect(self.reset_btn_click)
         self.answer_checkbox.clicked.connect(self.show_answer_checkbox_click)
+
+        #Game object
+        self.Game = Game_1A2B.Game()
+        self.guessCount = 0
+        self.get_focus()
+        self.confirm_btn.setDefault(True)
     
     def confirm_btn_click(self):
         self.guessNum = self.input_textbox.text()
         self.input_textbox.clear()
-        self.A,self.B,self.statusCode = Game_1A2B.Game(self.guessNum)
+        self.get_focus()
+        self.statusCode = self.Game.start(self.guessNum)
+        self.A , self.B = str(self.Game.A) , str(self.Game.B)
+
         if self.statusCode == -1:
-            self.MsgBox.setText('請輸入四位數字')
+            self.MsgBox.setWindowIcon(QtGui.QIcon('GUI/image/error_icon.png'))
+            self.MsgBox.setText('請輸入四位數字!')
             self.MsgBox.exec()
-        elif self.statusCode == 1:
-            self.MsgBox.setText('恭喜你猜對了!')
-            self.result_label.setText('4A0B')
-            self.MsgBox.exec()
-            self.reset_btn_click()
         else :
-            self.result_label.setText(str(self.A)+'A'+str(self.B)+'B')
+            self.guessCount = self.guessCount + 1
+            self.result_label.setText(self.A + 'A' + self.B + 'B')
+            self.log_list.addItem(str(self.guessCount) + ',' + self.guessNum + ',' + self.A + 'A' + self.B + 'B')
+            if self.statusCode == 1:
+                self.MsgBox.setWindowIcon(QtGui.QIcon('GUI/image/correct_icon.png'))
+                self.MsgBox.setText('恭喜你猜對了!')
+                self.result_label.setText('4A0B')
+                self.MsgBox.exec()
+
 
     def show_answer_checkbox_click(self):
         if self.answer_checkbox.isChecked():
             self.answer_label.setHidden(False)
-            self.answer_label.setText(Game_1A2B.returnAnswer())
+            self.answer_label.setText(self.Game.returnAnswer())
         else :
             self.answer_label.setHidden(True)
 
     def reset_btn_click(self):
-        Game_1A2B.reset()
+        self.Game.reset()
+        self.Game.returnAnswer()
+        self.get_focus()
+        self.guessCount = 0
         self.result_label.setText('0A0B')
+        self.answer_label.setHidden(True)
+        self.answer_checkbox.setChecked(False)
         self.input_textbox.clear()
-        self.input_textbox.setFocus()
         self.log_list.clear()
-
-    
         
-
-
-
+    def get_focus(self):
+        self.input_textbox.setFocus()
+        self.confirm_btn.setFocus()
--- Game_1A2B.py
+++ Game_1A2B.py
@@ -1,45 +1,59 @@
 import random
 from GUI.GUI import *
 
-#1A2B Game
-global rndNum
-rndNum = random.randint(1000,9999)
-def Game(guessNum):
-    A,B = 0,0
-    strNum = str(rndNum)
-    returnAnswer()
+"""
+1A2B Game 
 
-    if(len(guessNum) != 4):
-        return None,None,-1
-    else:
-        for k in range(4):
-            if guessNum[k] == strNum[k]:
-                A+=1
-                tempGuess = list(guessNum)
-                tempStr = list(strNum)
-                tempGuess[k] = '@'
-                tempStr[k] = '!'
-                guessNum = ''.join(tempGuess)
-                strNum = ''.join(tempStr)
-        for i in range(4):
-            for j in range(4):
-                    if guessNum[i] == strNum[j]:
-                        B+=1 
-                        tempGuess = list(guessNum)
-                        tempStr = list(strNum)
-                        tempGuess[i] = '@'
-                        tempStr[j] = '!'
-                        guessNum = ''.join(tempGuess)
-                        strNum = ''.join(tempStr)
-        if  A == 4:
-            return A,B,1
+A game that guess four-digit number
+
+Status Code:
+-1  : input length illegal
+0   : the answer is not right, game should continue
+1   : the answer is right, game pass
+
+"""
+class Game():
+    def __init__(self):
+        self.rndNum = random.randint(1000,9999)
+        self.A = 0
+        self.B = 0
+    
+    def start(self,guessNum):
+        self.A , self.B = 0,0
+        strNum = str(self.rndNum)
+        if(len(guessNum) != 4 or str(guessNum).isdigit() != True):
+            return -1
         else:
-            return A,B,0
-        
+            for k in range(4):
+                if guessNum[k] == strNum[k]:
+                    self.A+=1
+                    tempGuess = list(guessNum)
+                    tempStr = list(strNum)
+                    tempGuess[k] = '@'
+                    tempStr[k] = '!'
+                    guessNum = ''.join(tempGuess)
+                    strNum = ''.join(tempStr)
+            for i in range(4):
+                for j in range(4):
+                        if guessNum[i] == strNum[j]:
+                            self.B+=1 
+                            tempGuess = list(guessNum)
+                            tempStr = list(strNum)
+                            tempGuess[i] = '@'
+                            tempStr[j] = '!'
+                            guessNum = ''.join(tempGuess)
+                            strNum = ''.join(tempStr)
+            if  self.A == 4:
+                return 1
+            else:
+                return 0
+            
 
-def returnAnswer():
-    result = '答案:' + str(rndNum)
-    return result
+    def returnAnswer(self):
+        answer = '答案:' + str(self.rndNum)
+        return answer
 
-def reset():
-    rndNum = random.randint(1000,9999)
+    def reset(self):
+        self.rndNum = random.randint(1000,9999)
+        self.A = 0
+        self.B = 0
