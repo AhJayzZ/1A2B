8be8a045db4e8acd38b033c6c65ed91a317dc3e3
AhJayzZ <antony95070@gmail.com>
2021-10-18 14:13:40 +0800

    add BGM

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,9 +1,10 @@
-import sys
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from GUI.Ui_1A2B_GUI import *
+import sys,pygame
 import Game_1A2B
 
 class MainWindow(QtWidgets.QMainWindow,Ui_GameGUI):
@@ -15,22 +16,32 @@ class MainWindow(QtWidgets.QMainWindow,Ui_GameGUI):
         self.MsgBox = QtWidgets.QMessageBox()
         self.MsgBox.setWindowTitle('訊息')
 
-        # Checkbox setting
+        # Global setting
         self.answer_label.setStyleSheet("Color:Red")
+        self.confirm_btn.setDefault(True)
         self.answer_label.setHidden(True)
+        self.bgm_checkbox.setChecked(True)
         self.input_textbox.setMaxLength(4)
+        self.get_focus()
+        self.playBGM()
 
         # button trigger
         self.exit_btn.clicked.connect(sys.exit)
         self.confirm_btn.clicked.connect(self.confirm_btn_click)
         self.reset_btn.clicked.connect(self.reset_btn_click)
         self.answer_checkbox.clicked.connect(self.show_answer_checkbox_click)
+        self.bgm_checkbox.clicked.connect(self.playBGM)
+
+        # BGM setting
+        pygame.mixer.init()
+        pygame.mixer.music.load('GUI/bgm/bgm_1A2B.mp3')
+        pygame.mixer.music.play(-1,120,0)
+        pygame.mixer.music.set_volume(0.5)
 
         #Game object
         self.Game = Game_1A2B.Game()
         self.guessCount = 0
-        self.get_focus()
-        self.confirm_btn.setDefault(True)
+      
     
     def confirm_btn_click(self):
         self.guessNum = self.input_textbox.text()
@@ -75,3 +86,11 @@ class MainWindow(QtWidgets.QMainWindow,Ui_GameGUI):
     def get_focus(self):
         self.input_textbox.setFocus()
         self.confirm_btn.setFocus()
+
+    def playBGM(self):
+        pygame.mixer.init()
+        if self.bgm_checkbox.isChecked():
+            pygame.mixer.music.unpause()
+        else :
+            pygame.mixer.music.pause()
+        
